3051eb94045f943e1564d65ea517694743f8682e
Andrei Zhvaleuski <zhvaleuski.andrei@gmail.com>
2024-07-27 08:50:11 +0300

    Add tailwind css

--- app/web-app/src/App.tsx
+++ app/web-app/src/App.tsx
@@ -37,10 +37,10 @@ const Counter = observer(({ counterStore }: { counterStore: CounterStore }) => {
 
   return (
     <div style={{padding: "10px"}}>
-      <button onClick={() => counterStore.increment()}>
+      <button onClick={() => counterStore.increment()} className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
         count is {counterStore.counter}
       </button>
-      <button onClick={() => counterStore.decrement()}>
+      <button onClick={() => counterStore.decrement()} className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
         decrement
       </button>
     </div>
