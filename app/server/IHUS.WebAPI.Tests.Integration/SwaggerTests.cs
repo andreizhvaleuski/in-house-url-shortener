94bfcf3e36f3c407defbcd6bd6c1c3e6c4055fda
Andrei Zhvaleuski <andrei.zhvaleuski@yandex.by>
2024-08-29 14:30:51 +0300

    updates

--- app/server/IHUS.WebAPI.Tests.Integration/SwaggerTests.cs
+++ app/server/IHUS.WebAPI.Tests.Integration/SwaggerTests.cs
@@ -2,20 +2,13 @@
 
 namespace IHUS.WebAPI.Tests.Integration;
 
-public sealed class SwaggerTests
-    : IClassFixture<StashboxWebApplicationFactory<Program>>
+public sealed class SwaggerTests(StashboxWebApplicationFactory<Program> factory)
+        : IClassFixture<StashboxWebApplicationFactory<Program>>
 {
-    private readonly StashboxWebApplicationFactory<Program> _factory;
-
-    public SwaggerTests(StashboxWebApplicationFactory<Program> factory)
-    {
-        _factory = factory;
-    }
-
     [Fact]
     public async Task CreateShortUrl_ShouldReturnShortUrl_WhichCanBeUsedToAccessActualUrl()
     {
-        var client = _factory.StashClient((services, httpClientOptions) =>
+        var client = factory.StashClient((services, httpClientOptions) =>
         {
             httpClientOptions.BaseAddress = new Uri("http://localhost/");
         });
